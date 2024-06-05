f735d09dd7f30e769be7364fb50cc5f97b008770
Abby Vath Meyer <avmey@ucsb.edu>
2024-06-05 23:17:07 +0000

    Add rock island report

--- src/components/RepresentationPage.tsx
+++ src/components/RepresentationPage.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Kelp } from "./Kelp.js";
 import { KelpMax } from "./KelpMax.js";
+import { RockIslands } from "./RockIslands.js";
 
 const ReportPage = () => {
   return (
     <>
       <KelpMax />
       <Kelp />
+      <RockIslands />
     </>
   );
 };
