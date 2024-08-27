ef2d42d2256be5c5b5febe2879bb03007e530cd5
Abby Vath Meyer <avmey@ucsb.edu>
2024-08-27 23:01:36 +0000

    Small text changes

--- src/components/RepresentationPage.tsx
+++ src/components/RepresentationPage.tsx
@@ -1,21 +1,19 @@
 import React from "react";
 import { KelpMax } from "./KelpMax.js";
 import { Shoretypes } from "./Shoretypes.js";
-// import { Habitat } from "./Habitat.js";
 import { Estuaries } from "./Estuaries.js";
 import { Substrate } from "./Substrate.js";
 import { Eelgrass } from "./Eelgrass.js";
 import { KelpPersist } from "./KelpPersist.js";
-import { Bathymetry } from "./Bathymetry.js";
+import { Depth } from "./Depth.js";
 
 const ReportPage = () => {
   return (
     <>
-      <Bathymetry />
+      <Depth />
       <KelpMax />
       <KelpPersist />
       <Shoretypes />
-      {/* <Habitat /> */}
       <Estuaries />
       <Eelgrass />
       <Substrate />
