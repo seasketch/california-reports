e715b4be67a9ac5a59f382f36f5799cb7fe2dc98
Abby Vath Meyer <avmey@ucsb.edu>
2024-09-11 14:52:30 +0000

    Spacing updates

--- src/components/SpacingPage.tsx
+++ src/components/SpacingPage.tsx
@@ -1,8 +1,5 @@
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
-import { SpacingKelp } from "./SpacingKelp.js";
-import { SpacingEstuaries } from "./SpacingEstuaries.js";
-import { SpacingRockIslands } from "./SpacingRockIslands.js";
 import {
   Card,
   InfoStatus,
